a4f3b994181b5eadf2a1f83eda0f36e3fd2a2bb8
Thomasyan <wycwykds@163.com>
2017-12-23 12:39:56 +0800

    Update 2.ino

--- V2.0/2/2.ino
+++ V2.0/2/2.ino
@@ -2,7 +2,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
 const char* ssid = "*******";//路由器ssid
@@ -15,11 +14,14 @@ PubSubClient client(espClient);
 long lastMsg = 0;//存放时间的变量
 char msg[200];//存放要发的数据
 String load;
+int var = 0;
 //全局变量区域上界
 
 //全局变量区域下界
 
-void setup_wifi() {//自动连WIFI接入网络
+
+//自动连WIFI接入网络
+void setup_wifi() {
   delay(10);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -29,12 +31,14 @@ void setup_wifi() {//自动连WIFI接入网络
 }
 
 
-void reconnect() {//等待，直到连接上服务器
+//等待，直到连接上服务器
+void reconnect() {
   while (!client.connected()) {//如果没有连接上
     int randnum = random(0, 999);
     if (client.connect("DEMO" + randnum)) { //接入时的用户名，尽量取一个很不常用的用户名
       client.subscribe("inTopic");//接收外来的数据时的intopic
-    } else {
+    }
+    else {
       Serial.print("failed, rc=");//连接失败
       Serial.print(client.state());//重新连接
       Serial.println(" try again in 5 seconds");//延时5秒后重新连接
@@ -58,24 +62,20 @@ void  decodeJson() {
   DynamicJsonBuffer jsonBuffer;
   JsonObject& root = jsonBuffer.parseObject(load);
   var = root["var"];
-
   //接收数据json处理区上界
 
   //添加其他自己的JSON收听处理方式就像这样  int temperature=root["temperature"];
 
   //接收数据json处理区下界
-
 }
 
 
 void setup() {
-
   //setup代码区域上界
 
-  //填写自己的逻辑代码
+  //Your Code
 
   //setup代码区域下界
-
   Serial.begin(9600);
   setup_wifi();//自动连WIFI接入网络
   client.setServer(mqtt_server_ip, 1883);//1883为端口号
@@ -88,27 +88,23 @@ void loop() {
     reconnect();//确保连上服务器，否则一直等待。
   }
   client.loop();//MUC接收数据的主循环函数。
-
   //loop代码上届
 
-  //自己的逻辑代码
+  //Your Code
 
   //loop代码下界
-
   long now = millis();//记录当前时间
-  if (now - lastMsg > 100) {//每隔100毫秒秒发一次数据
+  if (now - lastMsg > 1000) {//每隔1000毫秒秒发一次数据
     encodeJson();
     client.publish("one", msg); //以one为TOPIC对外发送MQTT消息，这里的one需要修改成您的Topic
     lastMsg = now;//刷新上一次发送数据的时间
   }
-}
 }
 
 
 void encodeJson() {
   DynamicJsonBuffer jsonBuffer;
   JsonObject& root1 = jsonBuffer.createObject();
-
   //发送数据区上界
 
   //添加其他要发送的JSON包就像这样下面这句代码
